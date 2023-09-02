f9e220b6760c3ad769c88e86a979ba2002698cb0
RobertMcRae <robert.mcrae@mail.mcgill.ca>
2023-09-01 21:39:43 -0400

    checking to see if the changeswork  on my website

---
+++ app.js
@@ -0,0 +1,11 @@
+const computerChoice = document.getElementById("computer-choice")
+const userChoiceDisplay = document.getElementById("user-choice")
+const resultDisplay = document.getElementById("result")
+const possibleChoices = document.querySelectorAll('button') //picks all the buttons
+let userChoice
+
+
+possibleChoice.forEach(x => x.addEventlistener('click', (e) => {
+    userChoice = e.target.id
+    userChoiceDisplay.innerHTML = userChoice //on each click, the user choice inner html is updated 
+}))
