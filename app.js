c7a849d1df3a419edfc0308b28336e6a28c47a8b
RobertMcRae <robert.mcrae@mail.mcgill.ca>
2023-09-01 22:00:51 -0400

    testing to see if the preliminary app works

--- app.js
+++ app.js
@@ -3,9 +3,45 @@ const userChoiceDisplay = document.getElementById("user-choice")
 const resultDisplay = document.getElementById("result")
 const possibleChoices = document.querySelectorAll('button') //picks all the buttons
 let userChoice
+let result
 
 
 possibleChoices.forEach(x => x.addEventlistener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice //on each click, the user choice inner html is updated 
+    generateComputerChoice()
+    getResult()
 }))
+
+generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3)
+    switch (randomNumber) {
+        case 1: computerChoice  = 'rock'
+        case 2: computerChoice = 'scissors'
+        case 3: computerChoice = 'paper'
+    }
+}
+
+function getResult() {
+    if (computerChoice === userChoice){
+        result = `it's a draw!`
+    }
+    if (computerChoice === 'rock' && userChoice === 'paper'){
+        result =`you win!`
+    }
+    if (computerChoice === 'rock' && userChoice === 'scissors'){
+        result =`you lost!`
+    }
+    if (computerChoice === 'paper' && userChoice === 'rock'){
+        result =`you lost!`
+    }
+    if (computerChoice === 'paper' && userChoice === 'scissors'){
+        result =`you win!`
+    }
+    if (computerChoice === 'scissors' && userChoice === 'paper'){
+        result =`you lost!`
+    }
+    if (computerChoice === 'scissors' && userChoice === 'rock'){
+        result =`you win!`
+    }
+}
