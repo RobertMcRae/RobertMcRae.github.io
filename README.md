0a73764f2f4205a5b702fbfdaa4d7a59082d7737
RobertMcRae <robert.mcrae@mail.mcgill.ca>
2023-09-09 14:21:33 -0400

    added more features to the reveal cards function - flips over after 5 secodns

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ Memory Game:
 -you get __ lives per level
 -When you pass to the next level, there is either more pictures or less time to remember 
 -For a given level, when you get 3 matches, you are showed the cards again 
+
+
+Sep 1st: (1 hour) 
+Rock paper scissor game:
+-Implemented the user and computer choice, the result and the score
+
+Sept 3rd: (1 hour) 
+Memory Game:
+-Set up the html and the imageArray with the paths to the soruces 
+
+Sept 5th: (1 hour) 
+-Set up the blank cards
+-revealCards function which reveals all the hidden cards when a button is clicked
+
+Sept 7th: (30 min :( )
+-Duplicated the items 
+-reveal cards method now turns over after 5 seconds 
+-Started thinking about adding event listeners to the images 
+
+Sept 8th: ()
+-something weird is happening with reveal cards, my storeSet is not remobving my attributes 
--- memoryGame.js
+++ memoryGame.js
@@ -1,5 +1,8 @@
 const grid = document.getElementById('grid')
 
+let focusedQueue = []
+let storedSet = {}
+
 const imageArray = [
     {name: 'burger',
     source: 'memoryGameImages/burger.jpg',},
@@ -33,7 +36,7 @@ const imageArray = [
 ]
 
 let counter = 0
-for (let i=0; i<imageArray.length; i++) {
+for (let i=0; i<imageArray.length * 2; i++) {
     const imageData = document.createElement('img')
     imageData.src = 'memoryGameImages/questionMark.jpeg'
     imageData.style.width = "200px";
@@ -42,14 +45,46 @@ for (let i=0; i<imageArray.length; i++) {
     counter++
     grid.appendChild(imageData)
 }
+let imgElements = grid.querySelectorAll('img')
+imgElements.forEach(x => x.addEventListener('click', function() {
+    focusedQueue.push(x.id)
+    if (focusedQueue.length > 1) {
+        if (storedSet[focusedQueue[0]][0] === storedSet[focusedQueue[1]][0]) {
+            alert('You got a match')
+            grid.removeChild(document.getElementById(focusedQueue[0]))
+            grid.removeChild(document.getElementById(focusedQueue[1]))
+            delete storedSet.focusedQueue[0]
+            delete storedSet.focusedQueue[1]
+
+        }
+        else {
+            alert('You messed up')
+            focusedQueue = []
+        }
+        focusedQueue = []
+    }
+
+}))
+
+function turnGrid(grid){
+    let children = grid.children 
+    for (element of children) {
+        if (element.tagName === 'IMG') {
+            element.src = 'memoryGameImages/questionMark.jpeg'
+        }
+    }
+}
 
 function randomSortImages() {
-    randomNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-    let storedSet = {}
+    randomNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27]
+    //let storedSet = {}
     for (image of imageArray) {
-        let index = Math.floor(Math.random() * randomNumbers.length)
-        let numReturned = randomNumbers.splice(index,1)
-        storedSet[numReturned] = [image.name, image.source]
+        let index1 = Math.floor(Math.random() * randomNumbers.length)
+        let numReturned1 = randomNumbers.splice(index1,1)
+        storedSet[numReturned1] = [image.name, image.source]
+        let index2 = Math.floor(Math.random() * randomNumbers.length)
+        let numReturned2 = randomNumbers.splice(index2,1)
+        storedSet[numReturned2] = [image.name, image.source]
     }
     return storedSet
 }
@@ -57,16 +92,22 @@ function randomSortImages() {
 function revealCards(object) {
     for (idNumber in object) {
         imageElement = document.getElementById(idNumber)
-        console.log(object[idNumber][1])
         imageElement.src = object[idNumber][1]
     }
+    setTimeout(() => {
+        turnGrid(grid)
+    },5000)
 
 }
+
 
 let object1 = randomSortImages()
 
 document.getElementById('reveal').addEventListener('click', function() {
-    revealCards(object1)
+    console.log(storedSet) 
+    revealCards(storedSet)
 })
+
+
 
 //in the for list, add an id of 1,2,3 as i iterate through it and then randomly assign a number to the pictures 
